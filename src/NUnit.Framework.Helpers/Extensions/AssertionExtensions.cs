03297baf2ec40a3debd2e6b76d56d3f50da7fbae
Martin Buberl <mail@martinbuberl.com>
2012-01-21 22:21:58 -0500

    Add unit testing project; Add AssertionExtensions class;

---
+++ src/NUnit.Framework.Helpers/Extensions/AssertionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnit.Framework.Helpers.Extensions
+{
+    public static class AssertionExtensions
+    {
+    }
+}
