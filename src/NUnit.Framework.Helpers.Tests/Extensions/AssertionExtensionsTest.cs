6599fe181e7acacf6509c6812f29c27dffe4adc6
Martin Buberl <mail@martinbuberl.com>
2012-01-21 22:37:33 -0500

    Add AssertionExtensionsTest class

---
+++ src/NUnit.Framework.Helpers.Tests/Extensions/AssertionExtensionsTest.cs
@@ -0,0 +1,12 @@
+﻿namespace NUnit.Framework.Helpers.Tests.Extensions
+{
+    [TestFixture]
+    public class AssertionExtensionsTest
+    {
+        [Test]
+        public void ShouldBeNull_IsNull_ShouldPass()
+        {
+            
+        }
+    }
+}
