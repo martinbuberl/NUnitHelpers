1b56b9c41dcb4c5beff5b96822bc7f6dc753cea6
Martin Buberl <mail@martinbuberl.com>
2012-01-21 22:57:16 -0500

    Add ShouldBeNull, ShouldNotBeNull, ShouldBeNullOrEmpty, ShouldNotBeNullOrEmpty extensions and tests

--- src/NUnit.Framework.Helpers.Tests/Extensions/AssertionExtensionsTest.cs
+++ src/NUnit.Framework.Helpers.Tests/Extensions/AssertionExtensionsTest.cs
@@ -1,4 +1,7 @@
-﻿namespace NUnit.Framework.Helpers.Tests.Extensions
+﻿using System;
+using NUnit.Framework.Helpers.Extensions;
+
+namespace NUnit.Framework.Helpers.Tests.Extensions
 {
     [TestFixture]
     public class AssertionExtensionsTest
@@ -6,7 +9,51 @@
         [Test]
         public void ShouldBeNull_IsNull_ShouldPass()
         {
-            
+            ((string) null).ShouldBeNull();
+        }
+
+        [Test]
+        public void ShouldBeNull_IsNotNull_ShouldThrowAssertionException()
+        {
+            Assert.Throws<AssertionException>(() => String.Empty.ShouldBeNull());
+        }
+
+        [Test]
+        public void ShouldNotBeNull_IsNotNull_ShouldPass()
+        {
+            String.Empty.ShouldNotBeNull();
+        }
+
+        [Test]
+        public void ShouldNotBeNull_IsNull_ShouldThrowAssertionException()
+        {
+            Assert.Throws<AssertionException>(() => ((string) null).ShouldNotBeNull());
+        }
+
+        [Test]
+        public void ShouldBeNullOrEmpty_IsNullOrEmpty_ShouldPass()
+        {
+            ((string) null).ShouldBeNullOrEmpty();
+            String.Empty.ShouldBeNullOrEmpty();
+        }
+
+        [Test]
+        public void ShouldBeNullOrEmpty_IsNotNullOrEmpty_ShouldThrowAssertionException()
+        {
+            Assert.Throws<AssertionException>(() => "foo".ShouldBeNullOrEmpty());
+        }
+
+        [Test]
+        public void ShouldNotBeNullOrEmpty_IsNotNullOrEmpty_ShouldPass()
+        {
+            "foo".ShouldNotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void ShouldNotBeNullOrEmpty_IsNullOrEmpty_ShouldThrowAssertionException()
+        {
+            Assert.Throws<AssertionException>(() => ((string)null).ShouldNotBeNullOrEmpty());
+            Assert.Throws<AssertionException>(() => String.Empty.ShouldNotBeNullOrEmpty());
         }
     }
 }
--- src/NUnit.Framework.Helpers/Extensions/AssertionExtensions.cs
+++ src/NUnit.Framework.Helpers/Extensions/AssertionExtensions.cs
@@ -8,8 +8,22 @@ namespace NUnit.Framework.Helpers.Extensions
     {
         public static void ShouldBeNull(this object actual)
         {
-            // NUnit, VSTS
             Assert.IsNull(actual);
+        }
+
+        public static void ShouldNotBeNull(this object actual)
+        {
+            Assert.IsNotNull(actual);
+        }
+
+        public static void ShouldBeNullOrEmpty(this string actual)
+        {
+            Assert.IsNullOrEmpty(actual);
+        }
+
+        public static void ShouldNotBeNullOrEmpty(this string actual)
+        {
+            Assert.IsNotNullOrEmpty(actual);
         }
     }
 }
