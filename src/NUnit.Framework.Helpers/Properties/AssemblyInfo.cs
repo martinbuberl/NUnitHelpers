b9028e0c8ad3c990d8e25fe96bd8813551bf2386
Martin Buberl <mail@martinbuberl.com>
2012-01-23 22:53:31 -0500

    Changed assembly informations and set version to 0.1

--- src/NUnit.Framework.Helpers/Properties/AssemblyInfo.cs
+++ src/NUnit.Framework.Helpers/Properties/AssemblyInfo.cs
@@ -1,36 +1,35 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+#if DEBUG
+[assembly: AssemblyTitle("NUnit.Framework.Helpers (DEBUG)")]
+#else // RELEASE
 [assembly: AssemblyTitle("NUnit.Framework.Helpers")]
+#endif
+
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NUnit.Framework.Helpers")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("92ebe020-7fd9-4bc5-92d2-42a8137b7c74")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("NUnit.Framework.Helpers.Tests")]
+
+[assembly: AssemblyProduct("NUnit.Framework.Helpers")]
+
+[assembly: AssemblyCompany("Martin Buberl")]
+[assembly: AssemblyCopyright("Copyright (c) 2012, Martin Buberl")]
+[assembly: AssemblyTrademark("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else // RELEASE
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
