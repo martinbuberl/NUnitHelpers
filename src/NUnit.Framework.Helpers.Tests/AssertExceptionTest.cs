007f4ec0da65731bd4c41c848f4b7ef5164e4c27
Martin Buberl <mail@martinbuberl.com>
2012-01-23 22:46:56 -0500

    Add unit tests for Should.Throw<T>() methods

---
+++ src/NUnit.Framework.Helpers.Tests/AssertExceptionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NUnit.Framework.Helpers.Tests
+{
+    [TestFixture]
+    public class AssertExceptionTest
+    {
+        [Test]
+        public void ShouldThrowException_ThrowsException_ShouldPass()
+        {
+            Should.Throw<Exception>(ThrowsException);
+            Should.Throw<Exception>("foo", ThrowsException);
+        }
+
+        [Test]
+        public void ShouldThrowException_ThrowsNoException_ShouldFail()
+        {
+            Assert.Throws<AssertionException>(() => Should.Throw<Exception>(ThrowsNoException));
+            Assert.Throws<AssertionException>(() => Should.Throw<Exception>("foo", ThrowsNoException));
+        }
+
+        #region Helpers
+
+        public void ThrowsException()
+        {
+            throw new Exception("foo");
+        }
+
+        public void ThrowsNoException()
+        {
+        }
+
+        #endregion
+    }
+}
