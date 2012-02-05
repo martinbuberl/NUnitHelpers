9971c6b84628cba5bbea235b0d139fdf217f5684
Martin Buberl <mail@martinbuberl.com>
2012-02-04 21:23:39 -0500

    Made code more readable; Add info which files got deleted; Set error action to silent continue

--- ClickToClean.ps1
+++ ClickToClean.ps1
@@ -1,4 +1,9 @@
-Get-ChildItem . -recurse -force -include *.suo,*.user,*.cache,*.docstates,bin,obj,build,_ReSharper.* | foreach -process {Remove-Item $_.FullName -Force -Recurse}
+$itemsToDelete = Get-ChildItem . -recurse -force -include *.suo,*.user,*.cache,*.docstates,bin,obj,build,_ReSharper.*
+
+foreach ($item in $itemsToDelete) {
+    Remove-Item $item.FullName -Force -Recurse -ErrorAction SilentlyContinue
+    Write-Host "Deleted" $item.FullName
+}
 
 Write-Host "Press any key to continue . . ."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
